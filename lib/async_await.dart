bf368e42adb5509042bff73e8efbf80516a4e859
fanthasium <anhosung00@gmail.com>
2023-04-20 12:54:22 +0900

    동기, 비동기

---
+++ lib/async_await.dart
@@ -0,0 +1,21 @@
+
+
+Future<void> fetchUserOrder() {
+  // Imagine that this function is fetching user info from another service or database.
+
+  return Future.delayed(const Duration(seconds: 2), () => print('Large Latte'));
+}
+
+void main() async{
+
+  fetchUserOrder();
+  fetchUserOrder2(); // 룰루 랄라가 이후 출력되는데 !!
+  print("lulu lala");
+  Future.delayed(Duration(seconds: 1), () async => print( await 'Fetching user order...'));
+
+
+}
+
+fetchUserOrder2() async{
+   print(await "im the one!!");
+}
